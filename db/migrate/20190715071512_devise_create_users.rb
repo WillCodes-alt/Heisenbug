b3494b3c53f71328c8073b3b3faafbfde0213d9a
WillCodes-alt <will.work.ch@gmail.com>
2025-07-15 13:43:37 +0500

    work with migrations

--- db/migrate/20190715071512_devise_create_users.rb
+++ db/migrate/20190715071512_devise_create_users.rb
@@ -5,6 +5,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
+      t.string :full_name, null: false
+      t.integer :role
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -15,17 +17,17 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.inet     :current_sign_in_ip
-      # t.inet     :last_sign_in_ip
+       t.integer  :sign_in_count, default: 0, null: false
+       t.datetime :current_sign_in_at
+       t.datetime :last_sign_in_at
+       t.inet     :current_sign_in_ip
+       t.inet     :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+       t.string   :confirmation_token
+       t.datetime :confirmed_at
+       t.datetime :confirmation_sent_at
+       t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
--- db/migrate/20190715073052_create_projects.rb
+++ db/migrate/20190715073052_create_projects.rb
@@ -2,6 +2,11 @@ class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
 
+      t.string :title, null: false
+      t.string :description
+      t.references :creator
+      # if we use foreign_key =true rails will look for the table
+      # the other method is t.integer with foreign_key true
       t.timestamps
     end
   end
