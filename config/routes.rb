ca94df1bf72d571f7fa887579c572b78f024e387
WillCodes-alt <will.work.ch@gmail.com>
2025-07-17 15:15:11 +0500

    scaffoldings genereated

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
 
+  resource :projects do
+    resource :bugs do
+      resource :comments
+    end
+  end
 
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
