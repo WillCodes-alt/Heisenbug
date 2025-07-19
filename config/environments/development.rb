737f74f0ce5a5d6cac7a68d62b243955bbb5efcb
WillCodes-alt <will.work.ch@gmail.com>
2025-07-19 18:12:41 +0500

    with mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,17 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'localhost',
+      user_name:             'munnakhan0101@gmail.com',
+      password:              '',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
